113574be1775b056111d48db78317033b5a53c3c
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-18 13:12:25 +0530

    routes -defined|working

---
+++ yt-dashboard/backend/Routes/ApiRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+router.get('/testingendpoint', async(req,res) => {
+     try {
+        console.log('working here');
+        return res.status(200).json({
+            message : "Ending here"
+        })
+    } catch (error) {
+        console.log('working here',error);
+     }
+});
+
+router.get('/newone', async(req,res) => {
+     try {
+        console.log('working here');
+    } catch (error) {
+        console.log('working here',error);
+     }
+})
+
+export default router;
--- yt-dashboard/backend/index.js
+++ yt-dashboard/backend/index.js
@@ -1,10 +1,15 @@
+
 import express from 'express';
+import dotenv from 'dotenv';
+import router  from './Routes/ApiRoutes.js';
 
 const app = express();
-app.use(express.json())
+dotenv.config();
 
-const PORT  = 3000
+app.use(express.json());
 
-    app.listen(3000 , () => {                                                                                                   
-        console.log(`Server is listening on  ${PORT}`)
-    })
+app.use('/api/v1',router);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on  ${process.env.PORT}`)
+})
