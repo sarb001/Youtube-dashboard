c3219788897555ad9ac056eb8746c63b90264ede
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-18 13:58:59 +0530

    oauth urls working -login

--- yt-dashboard/backend/Routes/ApiRoutes.js
+++ yt-dashboard/backend/Routes/ApiRoutes.js
@@ -1,4 +1,4 @@
-
+import axios from 'axios';
 import express from 'express';
 const router = express.Router();
 
@@ -13,15 +13,49 @@ router.get('/testingendpoint', async(req,res) => {
      }
 });
 
-router.get('/newone', async(req,res) => {
+router.post('/login', async(req,res) => {
      try {
-        console.log('working newone ');
-          return res.status(200).json({
-            message : "Ending here new "
-        })
+         console.log('working login here ');
+         const OAuthUrl =  `${process.env.AUTH_URL}?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.Redirect_uri}&response_type=code&scope=${process.env.Scope}&access_type=offline&prompt=consent`;
+
+        console.log('Auth url is -',OAuthUrl);
+        return res.json({  url : OAuthUrl })
+        
     } catch (error) {
         console.log('working here',error);
+        return res.status(500).json({
+                message : "Redirected to Consent Screen"
+        })
      }
+})
+
+
+router.get('/callbackurl' , async(req,res) => {
+    try {
+           const code = req.query.code;
+        console.log('code query -',code);
+  
+        const Resp = await axios.post(process.env.Token_url,
+          {
+           code,
+           client_id : process.env.CLIENT_ID,
+           client_secret : process.env.CLIENT_SECRET,
+           redirect_uri: process.env.Redirect_uri,
+           grant_type : 'authorization_code',
+        }, { headers : { 'Content-Type' : 'application/json' }})
+  
+        const { access_token , refresh_token } = Resp?.data;
+        console.log(' tokens are =',access_token , refresh_token);
+
+
+        return res.status(200).redirect(`http://localhost:5173/dashboard?accesstoken=${access_token}&refreshtoken=${refresh_token}`)
+        
+    }catch(error){
+        console.log(' callback error =',error);
+        return res.status(500).json({
+          message : "oAuth Error"
+        })
+    }
 })
 
 export default router;
