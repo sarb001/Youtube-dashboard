e3afbbb0ca2e816b63d1063d2c3987896416e191
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-18 15:06:20 +0530

    set Title|desc -video

--- yt-dashboard/frontend/src/components/MainDashboard.jsx
+++ yt-dashboard/frontend/src/components/MainDashboard.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useState } from "react";
 import {  useSearchParams } from 'react-router-dom';
 
 const MainDashboard = () => {
@@ -7,11 +8,21 @@ const MainDashboard = () => {
 
     const AccToken = searchParams.get('accesstoken');
     const RefToken = searchParams.get('refreshtoken');
-    console.log('AccToken -',AccToken);
-    console.log('RefToken -',RefToken);
 
     localStorage.setItem('acctoken',AccToken);
     localStorage.setItem('refreshtoken',RefToken);
+
+    // const[videoDetails,setVideoDetails] = useState('');
+
+    const[Title,setTitle] = useState('');
+    const[Desc,setDesc] = useState('');
+
+    const[videoimg,setvideoimg] = useState(null);
+
+    // change title ,desc
+    // image
+    // write comment and reply it
+    /// delete comment as well
 
     const VideoHandler = async() => {
         try {
@@ -22,6 +33,13 @@ const MainDashboard = () => {
                 }
             })
             console.log('VideoResp =',VideoResp?.data?.videoinfo);
+            setTitle(VideoResp?.data?.videoinfo?.snippet?.title);
+            setDesc(VideoResp?.data?.videoinfo?.snippet?.description);
+            setvideoimg(VideoResp?.data?.videoinfo?.snippet.thumbnails?.default?.url);
+
+            console.log('title is -',Title);
+            console.log('desc is -',Desc);
+
         } catch (error) {
              console.log('video resp error-',error);
         }
@@ -30,11 +48,16 @@ const MainDashboard = () => {
     return (
         <div>
            <h2> Welcome to Personlized Youtube Dashboard </h2>
-           <div>
-             <button onClick={VideoHandler}> Fetch Video Details </button>
-           </div>
-           <div>
-              
+            <div>
+                <button onClick={VideoHandler}> Fetch Video Details </button>
+           <div style = {{padding:'25px'}}>
+                <div>
+                    <div> <img src = {videoimg} alt = "videoimage" /> </div>
+                        <h3> Title is - {Title} </h3>
+                        <h3> Description is - {Desc} </h3>
+                </div>
+             
+            </div>
            </div>
         </div>
     )
