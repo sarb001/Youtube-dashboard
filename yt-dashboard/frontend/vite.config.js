9192bb87ada303028095ecb9f719b69047d195bb
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-19 07:40:23 +0530

    loading prod var

--- yt-dashboard/frontend/vite.config.js
+++ yt-dashboard/frontend/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(),'');
+  const isProd = mode === 'production';
+
+  return {
   plugins: [react()],
   server : {
     proxy : {
       '/api/v1' : {
-         target : import.meta.env.VITE_API_BASE_URL,
+         target : isProd ? env.VITE_API_BASE_URL : 'http://localhost:3000',
          changeOrigin : true,
          secure : false,
       }
     }
   }
+}
 })
+
