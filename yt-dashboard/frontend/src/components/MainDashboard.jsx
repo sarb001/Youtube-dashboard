c5999e394751afaf1dd86ab35071652aa94dbd9e
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-22 19:46:13 +0530

    new acc_token -working

--- yt-dashboard/frontend/src/components/MainDashboard.jsx
+++ yt-dashboard/frontend/src/components/MainDashboard.jsx
@@ -1,172 +1,238 @@
+
 import axios from "axios"
 import { useState } from "react";
-import {  useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import { BaseUrl } from "../App";
+import { useEffect } from "react";
+
+// search params => accesstoken , refreshtoken
+
+// => stored in localstorage
+
+// => passed in videoid and in others ....
+
+// => after 
+
 
 const MainDashboard = () => {
 
     const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
 
-    const AccToken = searchParams.get('accesstoken');
-    const RefToken = searchParams.get('refreshtoken');
+    const [Videodetails, setVideodetails] = useState({
+        Title: "",
+        Desc: "",
+        Categoryid: "",
+        channelid: "",
+        videoimg: null,
+        videoid: ""
+    });
 
-    localStorage.setItem('acctoken',AccToken);
-    localStorage.setItem('refreshtoken',RefToken);
+    const [newTitle, setNewTitle] = useState('');
+    const [newDesc, setNewDesc] = useState('');
+    const [isAuthenticated, setisAuthenticated] = useState(false);
+    const [newcomment, setnewcomment] = useState('');
+    const [allcomments, setallcomments] = useState([]);
 
-    const[Title,setTitle] = useState('');
-    const[Desc,setDesc] = useState('');
-    const[Categoryid,setCategoryid] = useState('');
-    const[videoid,setvideoid] = useState('');
-    const[channelid,setchannelid] = useState('');
-    const[videoimg,setvideoimg] = useState(null);
+    const[MainAccesstoken,setMainAccesstoken]  = useState('');
+    const Refreshtoken = localStorage.getItem('refreshtoken');
+    const Accesstoken  = localStorage.getItem('accesstoken');
 
-    const[newTitle,setNewTitle] = useState('');
-    const[newDesc,setNewDesc] = useState('');
+    useEffect(() => {
 
-    const[newcomment,setnewcomment] = useState('');
-    const[allcomments,setallcomments] = useState([]);
+        const AccToken = searchParams.get('accesstoken');
+        const RefToken = searchParams.get('refreshtoken');
+        const Expire   = searchParams.get('expiresin');
 
-    // write comment and reply it
-    /// delete comment as well
+        if (AccToken && RefToken) {
+            localStorage.setItem('accesstoken', AccToken);
+            localStorage.setItem('refreshtoken', RefToken);
 
-    const VideoHandler = async() => {
+            if (Expire > 0) {        // 65 > 0 // token is valid 
+                setisAuthenticated(true);
+                const timeout = setTimeout(() => {
+                    NewAccesstoken();
+                }, Expire * 1000);
+                return () => clearTimeout(timeout);
+            } else {
+                setisAuthenticated(false);
+                navigate('/')
+            }
+
+        } else {
+            navigate('/')
+        }
+    }, [searchParams])
+
+    const NewAccesstoken = async () => {
+            const Response = await axios.post(`${BaseUrl}/api/v1/refreshtoken`, {
+                Refreshtoken
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+        console.log('token response =', Response?.data?.access_token);
+        const NewAcctoken = Response?.data?.access_token;
+        setMainAccesstoken(NewAcctoken);
+        localStorage.setItem('accesstoken', NewAcctoken);
+    }
+
+
+    const VideoHandler = async () => {
         try {
-            const VideoResp = await axios.get(`${BaseUrl}/api/v1/videoid`,{
-                headers : {
-                    'Content-Type' : 'application/json',
-                    'Authorization' : `Bearer ${AccToken}`
+            const VideoResp = await axios.get(`${BaseUrl}/api/v1/videoid`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${Accesstoken}`
                 }
             })
-            console.log('VideoResp =',VideoResp?.data?.videoinfo);
-            setTitle(VideoResp?.data?.videoinfo?.snippet?.title);
-            setDesc(VideoResp?.data?.videoinfo?.snippet?.description);
-            setvideoimg(VideoResp?.data?.videoinfo?.snippet.thumbnails?.default?.url);
-            setchannelid(VideoResp?.data?.videoinfo?.snippet?.channelId)
-            setCategoryid(VideoResp?.data?.videoinfo?.snippet?.categoryId);
-            setvideoid(VideoResp?.data?.videoinfo?.id);
+            console.log('VideoResp =', VideoResp?.data?.videoinfo);
+
+            const Mainsnippet = VideoResp?.data?.videoinfo?.snippet;
+            const Mainvideoid = VideoResp?.data?.videoinfo?.id;
+
+            setVideodetails({
+                Title: Mainsnippet?.title,
+                Desc: Mainsnippet?.description,
+                Categoryid: Mainsnippet?.categoryId,
+                channelid: Mainsnippet?.channelId,
+                videoimg: Mainsnippet?.thumbnails?.default?.url,
+                videoid: Mainvideoid
+            })
 
         } catch (error) {
-             console.log('video resp error-',error);
+            console.log('video resp error-', error);
         }
     }
 
-    const DetailsChangeHandler = async() => {
+    const DetailsChangeHandler = async () => {
         try {
 
-            if(Categoryid && videoid){
-                const Response = await axios.put(`${BaseUrl}/api/v1/contentdetails`,{
-                    newTitle,newDesc,Categoryid,videoid
-                },{
-                    headers : {
-                        'Content-Type' : 'application/json',
-                        'Authorization' : `Bearer ${AccToken}`    
+            if (Categoryid && videoid) {
+                const Response = await axios.put(`${BaseUrl}/api/v1/contentdetails`, {
+                    newTitle, newDesc, Categoryid, videoid
+                }, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${MainAccesstoken}`
                     }
                 });
-                console.log('Response =',Response);
+                console.log('Response =', Response);
                 setNewTitle("");
                 setNewDesc("");
             }
 
-       }catch(error) {
-          console.log('Response errror =',error);
-        }
-    }
-
-    const AllCommenthandler = async() => {
-
-        try {
-            const Response = await axios.get(`${BaseUrl}/api/v1/allcomments`,{
-                  headers : {
-                    'Content-Type' : 'application/json',
-                    'Authorization' : `Bearer ${AccToken}`
-                  }
-            });
-
-            console.log('Response =',Response?.data?.allcomments);
-            setallcomments(Response?.data?.allcomments);
-
         } catch (error) {
-            console.log('error =',error);
+            console.log('Response errror =', error);
         }
     }
 
-    const newcommenthandler = async() => {
-        console.log('channel id =',channelid);
+    const AllCommenthandler = async () => {
 
-        try {
-            const Response = await axios.post(`${BaseUrl}/api/v1/newcomment`, {
-                newcomment ,channelid,videoid
-            } , {
-                headers : {
-                       'Content-Type' : 'application/json',
-                       'Authorization' : `Bearer ${AccToken}`
+        console.log('acc token =',Accesstoken);
+            if(isAuthenticated){
+                try {
+                const Response = await axios.get(`${BaseUrl}/api/v1/allcomments`, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${MainAccesstoken}`
                     }
-            });
-            console.log('Response comment =>',Response);
-            setnewcomment("");
-        } catch (error) {
-            console.log('new post comment error =',error);
-        }
-        
+                });
+
+                console.log('Response =', Response?.data?.allcomments);
+                setallcomments(Response?.data?.allcomments);
+                
+            } catch (error) {
+                console.log('error =', error);
+            }
+    }
+    }
+
+    const newcommenthandler = async () => {
+        console.log('channel id =', channelid);
+
+        // try {
+        //     const Response = await axios.post(`${BaseUrl}/api/v1/newcomment`, {
+        //         newcomment ,channelid,videoid
+        //     } , {
+        //         headers : {
+        //                'Content-Type' : 'application/json',
+        //                'Authorization' : `Bearer ${AccToken}`
+        //             }
+        //     });
+        //     console.log('Response comment =>',Response);
+        //     setnewcomment("");
+        // } catch (error) {
+        //     console.log('new post comment error =',error);
+        // }
+
     }
 
     return (
         <div>
-           <h2> Welcome to Personlized Youtube Dashboard </h2>
+            <h2> Welcome to Personlized Youtube Dashboard </h2>
             <div>
                 <button onClick={VideoHandler}> Fetch Video Details </button>
-           <div style = {{padding:'25px'}}>
-                <div>
-                    <div> <img src = {videoimg} alt = "videoimage" /> </div>
-                    <div style = {{margin:'15px'}}>
-                        <h3> Title is - {Title} </h3>
+                <div style={{ padding: '25px' }}>
+                    <div>
                         <div>
-                            <input type = "text" placeholder = "Enter New title..." value = {newTitle} 
-                            onChange={(e) => setNewTitle(e.target.value)}
+                            <div key={Videodetails?.videoid} style={{ margin: '15px' }}>
+                                <div> <img src={Videodetails?.videoimg} alt="videoimage" /> </div>
+                                <h3> Title is  {Videodetails?.Title} </h3>
+                                <h4>  Desc is = {Videodetails?.Desc} </h4>
+                            </div>
+                        </div>
+
+                        <div>
+                            <input type="text" placeholder="Enter New title..." value={newTitle}
+                                onChange={(e) => setNewTitle(e.target.value)}
                             />
-                         </div>
-                         <div>
-                            <input type = "text" placeholder = "Enter New Desc..." value = {newDesc} 
-                            onChange={(e) => setNewDesc(e.target.value)}
+                        </div>
+
+                        <div>
+                            <input type="text" placeholder="Enter New Desc..." value={newDesc}
+                                onChange={(e) => setNewDesc(e.target.value)}
                             />
-                           </div>
-                        <div style = {{padding:'10px'}}>
+                        </div>
+
+                        <div style={{ padding: '10px' }}>
                             <button onClick={DetailsChangeHandler}> Change Title & Desc </button>
                         </div>
+
                     </div>
-                        <h3> Description is - {Desc} </h3>
                 </div>
 
                 <div>
-                        <h3> All Comments </h3>
-                        <div>
-                            <button onClick={AllCommenthandler}> Get All Comments </button>
-                        </div>
-                        <div>
-                            {allcomments?.map((i,index) => {
-                                return (
-                                    <div key = {index}> 
-                                      {i?.textOriginal}  
-                                    </div>
-                                )
-                            })}
-                        </div>
+                    <h3> All Comments </h3>
+                    <div>
+                        <button onClick={AllCommenthandler}> Get All Comments </button>
+                    </div>
+                    <div>
+                        {allcomments?.map((i, index) => {
+                            return (
+                                <div key={index}>
+                                    {i?.textOriginal}
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
 
                 <div>
                     <h3> Add New Comment </h3>
                     <div>
-                            <input type = "text" placeholder="Enter new comment" 
+                        <input type="text" placeholder="Enter new comment"
                             value={newcomment} onChange={(e) => setnewcomment(e.target.value)}
-                            />
-                     </div>
-                     <div style = {{paddingTop:'4%'}}>
-                       <button onClick={newcommenthandler}> Add New Comment </button>
-                     </div>
+                        />
+                    </div>
+                    <div style={{ paddingTop: '4%' }}>
+                        <button onClick={newcommenthandler}> Add New Comment </button>
+                    </div>
                 </div>
-            
+
+
             </div>
-           </div>
         </div>
     )
 }
